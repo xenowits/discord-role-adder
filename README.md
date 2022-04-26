3a1546c9a32b52a3a73615328d065dd5252a27c8
xenowits <kumar.abhishek111b@gmail.com>
2022-04-26 16:57:20 +0530

    add final code

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ Steps:
 3. guildId can be copied by right clicking on the server icon.
 4. token is generated on the discord developer portal for the bot.
 5. Permissions are generated on developer portal, OAuth2 inside Settings. Mark `bot` and `applications.commands`.
+
+Data:
+1. Add a file named `discord_handles.txt` which MUST consist of discord tags (format: `name#4355`) each on a separate new line.
+
+Considerations:
+1. The program fetches all the members of a guild and caches it.
+2. This cache is later used to get the `discord ID` from the `discord tags`.
+3. If we only have `discord ID`s as input, this could have a huge performance boost. Though, not much for smaller guild sizes
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require("discord.js");
 const { token, guildId } = require("./config.json");
+const fs = require("fs");
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+  intents: [Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS],
+});
 
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
@@ -24,29 +27,57 @@ client.on("interactionCreate", async (interaction) => {
     // Get role object from guild. Guild is another name for a discord server.
     var guild = client.guilds.cache.get(guildId);
     const role = await guild.roles.fetch(roleId);
-    console.log("Rolename is", role.name);
+    console.log("Rolename:", role.name);
 
-    // Get ID from discord username
-    const userId = client.users?.cache.find(
-      (u) => u.tag === "xenowits#2679"
-    )?.id;
-    console.log("UserId is", userId);
+    // Read the list of discord usernames from disk
+    var usernames = fs
+      .readFileSync("./discord_handles.txt", "utf8")
+      .toString()
+      .split("\n");
 
-    // Get user object
-    const member = await guild.members.fetch(userId);
+    try {
+      // Get all members of a discord server (guild) and cache it
+      await guild.members.fetch({ cache: true, force: true });
+    } catch (err) {
+      console.error(err);
+    }
 
-    // First check if the user already has the role
-    if (member.roles.cache.has(roleId)) {
-      console.log("User already has the role");
-    } else {
-      // Add the given role to the user
-      member.roles.add(role).catch(console.error);
-      // member.roles.remove(role).catch(console.error);
+    // Loop through each username
+    for (i in usernames) {
+      let name = usernames[i];
+      console.log("name:", name);
+
+      // Get ID from discord usertag. Since, we already cache ALL the members of a guild,
+      // cache miss should not happen.
+      const userId = client.users?.cache.find((u) => u.tag === name)?.id;
+      console.log("UserId:", userId);
+
+      if (userId == null) {
+        console.log("cannot find userId");
+        continue;
+      }
+
+      // Get user object
+      const member = await guild.members.fetch(userId);
+
+      if (member == null) {
+        console.log("couldn't fetch member");
+        continue;
+      }
+
+      // First check if the user already has the role
+      if (member.roles.cache.has(roleId)) {
+        console.log("User already has the role");
+      } else {
+        // Add the given role to the user
+        member.roles.add(role).catch(console.error);
+        // member.roles.remove(role).catch(console.error);
+      }
     }
 
     await interaction.reply("Done!");
   } else {
-    console.log("no matching commands");
+    await interaction.reply("No matching commands");
   }
 });
 
